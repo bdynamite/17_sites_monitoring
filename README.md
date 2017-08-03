0a44076968eaaee3e6b56f48d9f62d705ddc2ec3
bdynamite <bdynamite@yandex.ru>
2017-08-03 04:07:18 +0300

    первая попытка

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+Check site status
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+Download whois.exe from https://technet.microsoft.com/ru-ru/sysinternals/whois.aspx and put it to C:/Windows
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+# Quick Start
+
+Example of script launch on Windows, Python 3.6:
+
+```#!bash
+
+$ python downloader.py
+# input example
+
+input urls file path: urls.txt
+
+# output example
+
+devman.org monitoring:
+    http status: 200
+    expiry date: 2017-08-28
+meduza.io monitoring:
+    http status: 200
+    expiry date: 2018-07-21
+arzamas.academy monitoring:
+    http status: 200
+    expiry date: 2017-08-24
+```
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,45 @@
-def load_urls4check(path):
-    pass
+import subprocess
+import requests
 
-def is_server_respond_with_200(url):
-    pass
 
-def get_domain_expiration_date(domain_name):
-    pass
+def load_urls4check(site_name):
+    http_status = is_server_respond_with_200(site_name)
+    expiration_date = get_domain_expiration_date(site_name)
+    return http_status, expiration_date
+
+
+def is_server_respond_with_200(site_name):
+    url = 'http://{}'.format(site_name)
+    return requests.get(url).status_code
+
+
+def get_domain_expiration_date(site_name):
+    data_string = subprocess.check_output('whois {}'.format(site_name)).decode()
+    registry_date_pos = data_string.find('Registry Expiry Date: ') + 22
+    return data_string[registry_date_pos:registry_date_pos + 10]
+
+
+def get_file_path():
+    return input('input urls file path: ')
+
+
+def get_urls(path):
+    with open(path, 'r') as file_urls:
+        return [x[:-1] for x in file_urls.readlines()]
+
+
+def print_status(site, status_data):
+    print('{} monitoring:'.format(site))
+    print(' '*4 + 'http status: {}'.format(status_data[0]))
+    print(' '*4 + 'expiry date: {}'.format(status_data[1]))
+
+
+def main():
+    file_path = get_file_path()
+    names_list = get_urls(file_path)
+    for site_name in names_list:
+        response = load_urls4check(site_name)
+        print_status(site_name, response)
 
 if __name__ == '__main__':
-    pass
+    main()
